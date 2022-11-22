341bad5ed43e8a718d5ecd6bd9ad8512bae112c1
Despacita21 <lan20a@acu.edu>
2022-11-21 21:34:12 -0600

    Initial Commit

---
+++ Roll Again V3-LN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Roll Again V3-LN
+//
+//  Created by Lauren Najera on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Roll Again V3-LNUITests/Roll_Again_V3_LNUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Roll_Again_V3_LNUITestsLaunchTests.swift
+//  Roll Again V3-LNUITests
+//
+//  Created by Lauren Najera on 11/21/22.
+//
+
+import XCTest
+
+final class Roll_Again_V3_LNUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
